b6179e80e01d22cfb3ec1b97130bc61cd6f94d19
Eduardo Fabri <eduardohfabri@gmail.com>
2024-09-13 08:11:33 -0300

    Inicializando metodos na classe Plataforma

--- src/netflixdosguri/plataforma/Plataforma.java
+++ src/netflixdosguri/plataforma/Plataforma.java
@@ -9,4 +9,24 @@ public class Plataforma {
     private String nome;
     private ArrayList<Usuario> usuarios;
     private ArrayList<Streaming> conteudos; // filmes e series
+
+    public void adicionarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    public void adicionarConteudo(Streaming conteudo) {
+        conteudos.add(conteudo);
+    }
+
+    public void exibirUsuarios() {
+        for (Usuario usuario : usuarios) {
+            System.out.println(usuario);
+        }
+    }
+
+    public void exibirConteudos() {
+        for (Streaming conteudo : conteudos) {
+            System.out.println(conteudo);
+        }
+    }
 }
