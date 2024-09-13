a02c7d92bdd216a9ca71d7b87bcf4fc70519cf8c
Eduardo Fabri <eduardohfabri@gmail.com>
2024-09-13 08:04:14 -0300

    Inicializando classe Plataforma

---
+++ src/netflixdosguri/plataforma/Plataforma.java
@@ -0,0 +1,12 @@
+package netflixdosguri.plataforma;
+
+import netflixdosguri.streaming.Streaming;
+import netflixdosguri.usuario.Usuario;
+
+import java.util.ArrayList;
+
+public class Plataforma {
+    private String nome;
+    private ArrayList<Usuario> usuarios;
+    private ArrayList<Streaming> conteudos; // filmes e series
+}
