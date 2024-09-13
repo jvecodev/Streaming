23ed4216639ba7ae1aa802b8881cecd225996079
Eduardo Fabri <eduardohfabri@gmail.com>
2024-09-13 08:38:26 -0300

    Removi um objeto que instancei na intenção de usar ele com data

--- src/netflixdosguri/usuario/Premium.java
+++ src/netflixdosguri/usuario/Premium.java
@@ -7,8 +7,13 @@ import java.text.SimpleDateFormat;
 public class Premium extends Usuario {
     private boolean downloadOffline;
 
-    public Premium(String nome, String email, String senha, SimpleDateFormat dataNascimento, Pagamento pagamento, boolean downloadOffline) {
+    public Premium(String nome, String email, String senha, String dataNascimento, Pagamento pagamento, boolean downloadOffline) {
         super(nome, email, senha, dataNascimento, pagamento);
+        this.downloadOffline = downloadOffline;
+    }
+
+    public Premium(String nome, String email, Pagamento pagamento, boolean downloadOffline) {
+        super(nome, email);
         this.downloadOffline = downloadOffline;
     }
 }
