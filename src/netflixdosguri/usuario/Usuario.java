f7467fd1e0a5974b3440f6e4a37af9c47a7abc2f
Eduardo Fabri <eduardohfabri@gmail.com>
2024-09-13 08:03:25 -0300

    Inicializando classe Usuario

---
+++ src/netflixdosguri/usuario/Usuario.java
@@ -0,0 +1,21 @@
+package netflixdosguri.usuario;
+
+import netflixdosguri.pagamento.Pagamento;
+
+import java.text.SimpleDateFormat;
+
+public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+    private SimpleDateFormat dataNascimento = new SimpleDateFormat("dd/MM/yyyy");
+    private Pagamento pagamento;
+
+    public Usuario(String nome, String email, String senha, SimpleDateFormat dataNascimento, Pagamento pagamento) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.dataNascimento = dataNascimento;
+        this.pagamento = pagamento;
+    }
+}
