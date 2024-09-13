9daf7cb539107abe3e53fd66e21f919b5847d1ff
Eduardo Fabri <eduardohfabri@gmail.com>
2024-09-13 08:38:30 -0300

    Removi um objeto que instancei na intenção de usar ele com data

--- src/netflixdosguri/usuario/Usuario.java
+++ src/netflixdosguri/usuario/Usuario.java
@@ -8,14 +8,19 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
-    private SimpleDateFormat dataNascimento = new SimpleDateFormat("dd/MM/yyyy");
+    private String dataNascimento;
     private Pagamento pagamento;
 
-    public Usuario(String nome, String email, String senha, SimpleDateFormat dataNascimento, Pagamento pagamento) {
+    public Usuario(String nome, String email, String senha, String dataNascimento, Pagamento pagamento) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
         this.dataNascimento = dataNascimento;
         this.pagamento = pagamento;
+    }
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
     }
 }
